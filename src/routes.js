12925b0bfd08cdb9fe218245dc3f7b26222c6734
CaioAReis <Caio.almeida16.ca@gmail.com>
2020-05-13 21:55:16 -0300

    Criação da primeira parte do Drawer

--- src/routes.js
+++ src/routes.js
@@ -1,26 +1,38 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-
-const AppStack = createStackNavigator();
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import Home from './pages/home';
+
+const HomeStack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const HomeStackScreen = ({navigation}) => (
+    <HomeStack.Navigator screenOptions={{
+        headerStyle: {
+            backgroundColor: '#39e676',
+            // backgroundColor: '#009387'
+        },
+        headerTintColor: '#2E2E2E',
+        headerTitleStyle: {
+            fontWeight: 'bold',
+            alignSelf: "center",
+        }
+    }} initialRouteName="Home">
+        <HomeStack.Screen name="Home" component={Home} />
+    </HomeStack.Navigator>
+);
+
 
 export default function Routes() {
     return (
         <NavigationContainer>
+            <Drawer.Navigator initialRouteName="Home">
+                <Drawer.Screen name="Home" component={HomeStackScreen} />
+            </Drawer.Navigator>
 
-            <AppStack.Navigator screenOptions={{
-                headerStyle: {
-                    backgroundColor: '#39e676',
-                },
-                headerTintColor: '#fff',
-                headerTitleStyle: {
-                    fontWeight: 'bold'
-                }
-            }} initialRouteName="Home">
-                <AppStack.Screen name="Home" component={Home} />
-            </AppStack.Navigator>
+
 
         </NavigationContainer>
     );
