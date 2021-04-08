3a9746f9c02bc88c0b7e7006b59bbe9beb37be73
CaioAReis <Caio.almeida16.ca@gmail.com>
2021-04-08 11:27:30 -0300

    Primeira tentativa de fazer a barra de pesquisa

--- src/components/customHeader/index.js
+++ src/components/customHeader/index.js
@@ -1,11 +1,17 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Animated } from 'react-native';
+import { SearchBar } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/AntDesign';
 
 import styles from './styles';
 
 export default function HeaderCustom({ navigation, title }) {
+
+    const alt = new Animated.Value(58);
+    let key = false;
+
     return(
+    <Animated.View style={{overflow: 'hidden',height: alt}}>    
         <View style={styles.customHeader}>
 
             <Icon.Button name='bars' size={30} color={'#FFF'}
@@ -16,9 +22,21 @@ export default function HeaderCustom({ navigation, title }) {
             <Text style={styles.customHeaderText}>{title}</Text>
 
             <Icon.Button name='search1' size={27} color={'#FFF'}
-                backgroundColor="#099e4f" onPress={() => {alert('PESQUISA')}}
-                style={{paddingLeft: 20}}>
+                backgroundColor="#099e4f" onPress={() => {
+                    key = !key;
+                    if (key) Animated.timing(alt,{toValue: 127, duration: 200 }).start();
+                    else Animated.timing(alt,{toValue: 58, duration: 200 }).start();}}
+                style={{}}>
             </Icon.Button>
         </View>
+
+        <View style={{width: '100%', height: 70}}>
+            <SearchBar 
+                placeholder="Pesquisa..."
+                lightTheme='true'
+                showLoading='true'
+            />
+        </View>
+    </Animated.View>
     );
 }
--- src/components/customHeader/styles.js
+++ src/components/customHeader/styles.js
@@ -2,11 +2,12 @@ import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
     customHeader:{
-        flexDirection: 'row',
-        backgroundColor: '#099e4f',
-        height: 57, 
+        // display: 'flex',
         justifyContent: 'space-between',
         alignItems: 'center',
+        flexDirection: 'row',
+        backgroundColor: '#099e4f',
+        height: 57,
         borderBottomWidth: 2,
         borderColor: '#04803d',
     },
@@ -15,4 +16,37 @@ export default StyleSheet.create({
         fontWeight: 'bold',
         fontSize: 20
     },
+    input: {
+        position: 'absolute',
+        height: 40,
+        width: 200,
+        // borderWidth: 1,
+
+        backgroundColor: 'white',
+        borderRadius: 6,
+        paddingLeft: 10,
+        fontSize: 18,
+        color: '#04803d',
+        lineHeight: 50,
+    },
+    pesquisaContainer: {
+
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+
+        position: 'relative',
+        left: 0, right: 0, top: 0, bottom: 0,
+        backgroundColor: 'rgba(0, 0, 0, 0.50)',
+        width: '100%',
+        height: '100%',
+        zIndex: 200
+    },
+    container: {
+        width: '80%',
+        height: '50%',
+        backgroundColor: '#FFF',
+        borderRadius: 8,
+        padding: 15
+    }
 });
